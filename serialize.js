bb8353fd28a5e80fa91039d1c2cc5fcf9f291cd1
kafkaacademy <info@kafka.academy>
2020-07-01 19:36:48 +0200

    stricter javascript

--- serialize.js
+++ serialize.js
@@ -140,124 +140,126 @@ function serialize(form, options) {
     }
 
     return result;
-}
 
-function parse_keys(string) {
-    var keys = [];
-    var prefix = /^([^\[\]]*)/;
-    var children = new RegExp(brackets);
-    var match = prefix.exec(string);
 
-    if (match[1]) {
-        keys.push(match[1]);
-    }
+    // Object/hash encoding serializer.
+    function hash_serializer(result, key, value) {
+        var matches = key.match(brackets);
 
-    while ((match = children.exec(string)) !== null) {
-        keys.push(match[1]);
-    }
-
-    return keys;
-}
-
-function hash_assign(result, keys, value) {
-    if (keys.length === 0) {
-        result = value;
-        return result;
-    }
-
-    var key = keys.shift();
-    var between = key.match(/^\[(.+?)\]$/);
-
-    if (key === '[]') {
-        result = result || [];
-
-        if (Array.isArray(result)) {
-            result.push(hash_assign(null, keys, value));
+        // Has brackets? Use the recursive assignment function to walk the keys,
+        // construct any missing objects in the result tree and make the assignment
+        // at the end of the chain.
+        if (matches) {
+            var keys = parse_keys(key);
+            hash_assign(result, keys, value);
         }
         else {
-            // This might be the result of bad name attributes like "[][foo]",
-            // in this case the original `result` object will already be
-            // assigned to an object literal. Rather than coerce the object to
-            // an array, or cause an exception the attribute "_values" is
-            // assigned as an array.
-            result._values = result._values || [];
-            result._values.push(hash_assign(null, keys, value));
-        }
+            // Non bracket notation can make assignments directly.
+            var existing = result[key];
 
+            // If the value has been assigned already (for instance when a radio and
+            // a checkbox have the same name attribute) convert the previous value
+            // into an array before pushing into it.
+            //
+            // NOTE: If this requirement were removed all hash creation and
+            // assignment could go through `hash_assign`.
+            if (existing) {
+                if (!Array.isArray(existing)) {
+                    result[key] = [existing];
+                }
+
+                result[key].push(value);
+            }
+            else {
+                result[key] = value;
+            }
+        }
         return result;
-    }
 
-    // Key is an attribute name and can be assigned directly.
-    if (!between) {
-        result[key] = hash_assign(result[key], keys, value);
-    }
-    else {
-        var string = between[1];
-        // +var converts the variable into a number
-        // better than parseInt because it doesn't truncate away trailing
-        // letters and actually fails if whole thing is not a number
-        var index = +string;
+        function parse_keys(string) {
+            var keys = [];
+            var prefix = /^([^\[\]]*)/;
+            var children = new RegExp(brackets);
+            var match = prefix.exec(string);
 
-        // If the characters between the brackets is not a number it is an
-        // attribute name and can be assigned directly.
-        if (isNaN(index)) {
-            result = result || {};
-            result[string] = hash_assign(result[string], keys, value);
-        }
-        else {
-            result = result || [];
-            result[index] = hash_assign(result[index], keys, value);
-        }
-    }
-    return result;
-}
-
-// Object/hash encoding serializer.
-function hash_serializer(result, key, value) {
-    var matches = key.match(brackets);
-
-    // Has brackets? Use the recursive assignment function to walk the keys,
-    // construct any missing objects in the result tree and make the assignment
-    // at the end of the chain.
-    if (matches) {
-        var keys = parse_keys(key);
-        hash_assign(result, keys, value);
-    }
-    else {
-        // Non bracket notation can make assignments directly.
-        var existing = result[key];
-
-        // If the value has been assigned already (for instance when a radio and
-        // a checkbox have the same name attribute) convert the previous value
-        // into an array before pushing into it.
-        //
-        // NOTE: If this requirement were removed all hash creation and
-        // assignment could go through `hash_assign`.
-        if (existing) {
-            if (!Array.isArray(existing)) {
-                result[key] = [existing];
+            if (match[1]) {
+                keys.push(match[1]);
             }
 
-            result[key].push(value);
+            while ((match = children.exec(string)) !== null) {
+                keys.push(match[1]);
+            }
+
+            return keys;
         }
-        else {
-            result[key] = value;
+
+        function hash_assign(result, keys, value) {
+            if (keys.length === 0) {
+                result = value;
+                return result;
+            }
+
+            var key = keys.shift();
+            var between = key.match(/^\[(.+?)\]$/);
+
+            if (key === '[]') {
+                result = result || [];
+
+                if (Array.isArray(result)) {
+                    result.push(hash_assign(null, keys, value));
+                }
+                else {
+                    // This might be the result of bad name attributes like "[][foo]",
+                    // in this case the original `result` object will already be
+                    // assigned to an object literal. Rather than coerce the object to
+                    // an array, or cause an exception the attribute "_values" is
+                    // assigned as an array.
+                    result._values = result._values || [];
+                    result._values.push(hash_assign(null, keys, value));
+                }
+
+                return result;
+            }
+
+            // Key is an attribute name and can be assigned directly.
+            if (!between) {
+                result[key] = hash_assign(result[key], keys, value);
+            }
+            else {
+                var string = between[1];
+                // +var converts the variable into a number
+                // better than parseInt because it doesn't truncate away trailing
+                // letters and actually fails if whole thing is not a number
+                var index = +string;
+
+                // If the characters between the brackets is not a number it is an
+                // attribute name and can be assigned directly.
+                if (isNaN(index)) {
+                    result = result || {};
+                    result[string] = hash_assign(result[string], keys, value);
+                }
+                else {
+                    result = result || [];
+                    result[index] = hash_assign(result[index], keys, value);
+                }
+            }
+            return result;
         }
     }
-    return result;
-}
 
-// urlform encoding serializer
-function str_serialize(result, key, value) {
-    if (typeof value === 'string') {
-        value = value.replace(/(\r)?\n/g, '\r\n');
-        value = encodeURIComponent(value);
 
-        // spaces should be '+' rather than '%20'.
-        value = value.replace(/%20/g, '+');
+    // urlform encoding serializer
+    function str_serialize(result, key, value) {
+        if (typeof value === 'string') {
+            value = value.replace(/(\r)?\n/g, '\r\n');
+            value = encodeURIComponent(value);
+
+            // spaces should be '+' rather than '%20'.
+            value = value.replace(/%20/g, '+');
+        }
+
+        return result + (result ? '&' : '') + encodeURIComponent(key) + '=' + value;
     }
-
-    return result + (result ? '&' : '') + encodeURIComponent(key) + '=' + value;
 }
 
 module.exports = serialize;
